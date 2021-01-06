88a46017d3ea0e38e368c8ca6b92062f483bd114
AnonRZZ <anonrzzgm@gmail.com>
2021-01-06 13:43:25 -0800

    Add option to show list of countries + error message

--- cogs/covidTracker.py
+++ cogs/covidTracker.py
@@ -17,8 +17,16 @@ class CovidTracker(commands.Cog):
             await ctx.send(csvDownloader().find_data(str(country_name)))
 
         except Exception as err:
-            await ctx.send("You might have inputted a country name that I cannot identify")
+            await ctx.send("You might have inputted a country name that I cannot identify. You can access the list of identified nations by typing '?covlist'")
             raise err
+
+    @commands.command(name="covlist", help="Find list of searchable nations for Covid19 data")
+    async def covlist (self, ctx):
+        print(csvDownloader().country_list())
+        half_length = len(csvDownloader().country_list()) // 2
+        await ctx.author.send(', '.join(csvDownloader().country_list()[:half_length]))
+        await ctx.author.send(', '.join(csvDownloader().country_list()[-half_length:]))
+        await ctx.author.send('You would need to copy and paste the full names of each nation when using command "covid"')
 
 def setup(bot):
     bot.add_cog(CovidTracker(bot))
--- utilities/CovidDownloader.py
+++ utilities/CovidDownloader.py
@@ -10,16 +10,33 @@ class csvDownloader():
 
 	def find_data(self, countryName):
 		
+
 		df = pd.read_csv(url)
+		global myDict 
 		myDict = df.to_dict()
 
+		global x
 		x = list(myDict['Name'].keys())[list(myDict['Name'].values()).index(countryName)]
 		
 		message = "\n"
 		for value in myDict:
-			print(value, myDict[f'{value}'][x])
+			
 			message += str(f"{value}: {myDict[f'{value}'][x]} \n")
 		
 		return str(message)
+
+	def country_list(self):
+		df = pd.read_csv(url)
+		global myDict 
+		myDict = df.to_dict()
+
+		clist = []
+		for value in myDict['Name'].values():
+			clist.append(str(f"{value}"))
+		
+		sorted_clist = sorted(clist, key=str.lower)
+		
+		
+		return sorted_clist
 
 url = r"https://covid19.who.int/WHO-COVID-19-global-table-data.csv"
